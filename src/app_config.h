e3b30cb4bfbfaf75e2b6ce817fedf67e69d50999
Mateusz K <m_karabela@o2.pl>
2019-04-10 21:58:30 +0200

    One .h file for some common app config

---
+++ src/app_config.h
@@ -0,0 +1,19 @@
+/*
+ * app_config.h
+ *
+ *  Created on: 22.03.2019
+ *      Author: E6430
+ */
+
+#ifndef APP_CONFIG_H_
+#define APP_CONFIG_H_
+
+#define SOCKETNO_DHCP			5
+#define SOCKETNO_CLI			6
+#define SOCKETNO_DISCOVERY		7
+
+#define PORTNO_CLI				1234
+#define PORTNO_DISCOVERY		1235
+
+
+#endif /* APP_CONFIG_H_ */
