655cd920e440f7289d95e76a8cb9973c74f7b7cb
Mateusz K <m_karabela@o2.pl>
2019-05-05 22:56:40 +0200

    Some brief documentation

--- src/utils/Console/Console.h
+++ src/utils/Console/Console.h
@@ -8,7 +8,27 @@
 #ifndef UTILS_CONSOLE_CONSOLE_H_
 #define UTILS_CONSOLE_CONSOLE_H_
 
-// This is a pixel-by-pixel, line-by-line app-specific console. Utilizes Adafruits GFX library GFXfont structure for storing fonts
+/** This is a pixel-by-pixel, line-by-line app-specific console. Utilizes Adafruits GFX library GFXfont structure for storing fonts
+*	Example usage:
+*
+*	// prepare buffer for characters
+*	char consolebuffer[8*25];
+*	// init console struct with some settings and font object
+*	sConsole console;
+*	ConsoleInit(&console, consolebuffer, 8, 25, 0, 6, 5, 8, TomThumb);
+*	// clear the buffer
+*	ConsoleClear(&console);
+*	// feed some characters
+*	ConsolePutChar(&console, 'a');
+*	// move the "cursor" to eg. column 3 and row 5
+*	ConsoleSetCursor(&console, 3, 5);
+*	// if you already have a buffer you want to display, feed it
+*	ConsoleFeedBuffer(&console, ready_buffer, its_size);
+*	// finally render it via pixel [x,y] interface provided in ConsoleSetPix(uint8_t x, uint8_t y)
+*	ConsoleRender(&console);
+*	// now it is up to display driver how to display it...
+*
+*/
 
 #include <stdio.h>
 #include <Console/gfxfont.h>
@@ -31,6 +51,21 @@ typedef struct {
 }sConsole;
 
 // Interface functions:
+
+/**
+ * ConsoleInit
+ * Initializes a console structure
+ * Params:
+ * *Console - pointer to empty sConsole struct that will be used
+ * *cBuffer - pointer to already allocated char buffer, big enough to accomodate all the console's characters
+ * uRows - describes how many rows of text a console can fit
+ * uColumns - as above, but columns
+ * uXStart, uYStart - starting point in pixels, it indicated a lower-left corner of first letter that will be rendered (column=0,row=0)
+ * uXAdvance - a fixed constant width in pixels of every character, should be large enough so characters do not overlap
+ * uYAdvance - a fixed constant distance in pixels between consecutive lines of text
+ * sFont - a struct containing font information
+ *
+ */
 sConsole* ConsoleInit(sConsole *Console, char *cBuffer, uint8_t uRows, uint8_t uColumns, uint8_t uXStart, uint8_t uYStart, uint8_t uXAdvance, uint8_t uYAdvance, GFXfont sFont);
 void ConsoleRender(sConsole *Console);
 void ConsoleSetCursor(sConsole *Console, uint8_t uColumn, uint8_t uRow);
